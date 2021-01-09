6b3e089b6a6c8005a129700dd199ffc5d34dfe0e
Osama Adam <osamaadam98@gmail.com>
2021-01-09 21:05:45 +0200

    first part

--- main.m
+++ main.m
@@ -3,23 +3,40 @@ clear;
 
 numOfBits = 1000;
 
-randomSeq = randi([0, 1], numOfBits, 1);
+randomSeq = randi([0, 1], numOfBits, 1) * 2 - 1;
 
 L = 10;
 
-hOrg = MultipathChannel(L, numOfBits);
+hOrg = transpose(MultipathChannel(L));
 
 h = tril(toeplitz(hOrg));
 hInv = inv(h);
 
-ebOverNo = [-9:0];
+ebOverNo = [-15:0.1:0];
 energyPerBit = 1;
-No = ones(1, length(ebOverNo))./((10.^(ebOverNo/10))*energyPerBit);
 
-noise = (No / 2) .* randn(size(randomSeq));
+for i = 1: length(ebOverNo)
+  noise = awgn(randomSeq, ebOverNo(i), energyPerBit);
 
-noise = transpose(mat2cell(noise, [1000], repelem([1], L)));
+  noiseCells = mat2cell(reshape(noise, 10, numOfBits / 10), [10], repelem([1], numOfBits / 10));
 
-Y = cellfun(@(x) h * randomSeq + double(x), noise, "UniformOutput", false);
+  ##noise = mat2cell(reshape(noise, 10, numOfBits / 10), [10], repelem([1], numOfBits / 10));
+  signal = mat2cell(reshape(randomSeq, 10, numOfBits / 10), [10], repelem([1], numOfBits / 10));
 
-X = cellfun(@(x) hInv * x, Y, "UniformOutput", false);
+  Y = cellfun(@(x, n) h * x + n, signal, noiseCells, "UniformOutput", false);
+  X = cellfun(@(y) hInv * y, Y, "UniformOutput", false);
+
+  sigMat = reshape(cell2mat(signal), numOfBits, 1);
+  xMat = reshape(cell2mat(X), numOfBits, 1);
+
+  meanVal = (max(xMat) + min(xMat)) / 2;
+
+  xMat = xMat >= meanVal;
+
+  ber(i) = (sum(xor((sigMat + 1) / 2, xMat)) / numOfBits) * 100;
+end
+
+figure;
+plot(ebOverNo, ber);
+ylabel("BER");
+xlim([-15,0]);
